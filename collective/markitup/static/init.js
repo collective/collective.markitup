707ef5998d5a617a4a31a883989960f041317b3e
msmith64 <msmith64>
2011-07-07 19:15:53 +0000

    jslint

--- collective/markitup/static/init.js
+++ collective/markitup/static/init.js
@@ -17,17 +17,28 @@
 //  http://markitup.jaysalvat.com/.
 //
 
-String.prototype.format = function() {
-	var formatted = this;
-	for (var i = 0; i < arguments.length; i++) {
-		var regexp = new RegExp('\\{'+i+'\\}', 'gi');
-		formatted = formatted.replace(regexp, arguments[i]);
+/*jslint browser: true, plusplus: true, maxerr: 50, indent: 4 */
+
+// Expected Globals
+var mySettings = mySettings || {},
+	portal_url = portal_url || "",
+	jQuery = jQuery || {};
+
+
+String.prototype.format = function () {
+	"use strict";
+	var formatted = this,
+		i = arguments.length,
+		regex;
+	while (i--) {
+		regex = new RegExp('\\{' + i.toString() + '\\}', 'gi');
+		formatted = formatted.replace(regex, arguments[i]);
 	}
 	return formatted;
 };
 
 // A namespace for anything to do with the MarkItUp editor.
-markitup = {
+var markitup = {
 
 	// The URL from which to load resources
 	base: portal_url + "/++resource++collective.markitup/",
@@ -40,12 +51,12 @@ markitup = {
 	//
 	// This is here for example only. It will be overridden by stuff
 	// in the plone registry.
-	format: {
+	formatStr: {
 		"text/x-web-markdown": {
-		// [URL, alternative text, title]
-		Picture: '![[![{1}]!]]([![Url:!:{0}]!] "[![{2}]!]")',
-		// [URL, link text, title]
-		Link: '[{1}]([![Url:!:{0}]!] "[![{2}]!]")'
+			// [URL, alternative text, title]
+			Picture: '![[![{1}]!]]([![Url:!:{0}]!] "[![{2}]!]")',
+			// [URL, link text, title]
+			Link: '[{1}]([![Url:!:{0}]!] "[![{2}]!]")'
 		}
 	},
 
@@ -54,12 +65,15 @@ markitup = {
 	 * by creating and attaching a script element.
 	 * @param url {string} Value of the src attribute of the element to create
 	 */
-	loadScript: function(url) {
-		if ($('script[src="'+url+'"]').length > 0) return;
+	loadScript: function (url) {
+		"use strict";
+		if (jQuery('script[src="' + url + '"]').length > 0) {
+			return;
+		}
 		var script = document.createElement('script');
 		script.type = 'text/javascript';
 		script.src = url;
-		$("head").append(script);
+		jQuery("head").append(script);
 	},
 
 	/**
@@ -67,27 +81,32 @@ markitup = {
 	 * creating and attaching a style element.
 	 * @param url {string} Value of the href attribute of the element to create
 	 */
-	loadStyle: function(url) {
-		if ($('link[href="'+url+'"]').length > 0) return;
+	loadStyle: function (url) {
+		"use strict";
+		if (jQuery('link[href="' + url + '"]').length > 0) {
+			return;
+		}
 		var style = document.getElementById("markitup-style");
-		if (!style) style = document.createElement("link");
+		if (!style) {
+			style = document.createElement("link");
+		}
 		style.id = "markitup-style";
 		style.rel = "stylesheet";
 		style.type = "text/css";
 		style.media = "screen";
 		style.href = url;
-		$("head").append(style);
+		document.head.appendChild(style);
 	},
 
 	/**
-	 * Detach the MarkItUp editor from the $("#text") element.
+	 * Detach the MarkItUp editor from the jQuery("#text") element.
 	 */
-	unloadSet: function() {
+	unloadSet: function () {
+		"use strict";
 		/* NOTE: Unloading the js and css directly appears to cause problems.
 		         Hopefully leaving them in place won't break anything.
 		*/
-		// $("#text").markItUpRemove();
-		$("textarea").markItUpRemove();
+		jQuery("#text").markItUpRemove();
 	},
 
 	// A namespace for anything having to do with the finder/browser/picker
@@ -95,17 +114,18 @@ markitup = {
 		/**
 		 * Open the finder in a new window.
 		 */
-		open: function(buttonSet) {
-			var target = $("."+buttonSet.className+">a");
-			if (buttonSet.name == "Picture") {
-				target.attr("href", portal_url+"/@@markitup_imagefinder");
+		open: function (buttonSet) {
+			"use strict";
+			var target = jQuery("." + buttonSet.className + ">a");
+			if (buttonSet.name === "Picture") {
+				target.attr("href", portal_url + "/@@markitup_imagefinder");
 			} else {
-				target.attr("href", portal_url+"/@@markitup_finder");
+				target.attr("href", portal_url + "/@@markitup_finder");
 			}
 			target.prepOverlay({
 				subtype: "iframe",
 				config: {
-					closeOnClick: false,
+					closeOnClick: false
 				}
 			});
 			markitup.finder.overlay = target.data("overlay");
@@ -117,15 +137,18 @@ markitup = {
 		 * and then closes the iframe.
 		 */
 		selectImage: function (UID, title) {
-			var statusBar = $(".statusBar > div", Browser.window);
-			var parent = window.parent;
-			if (window.opener) parent = window.opener;
+			"use strict";
+			var Browser = Browser || {},
+				statusBar = jQuery(".statusBar > div", Browser.window),
+				parent = window.parent,
+				src = portal_url + "/@@markitup_redirect_uid?uid=" + UID,
+				formatStr = markitup.formatStr["text/" + markitup.currentSet].Picture;
+			if (window.opener) {
+				parent = window.opener;
+			}
 			statusBar.hide().filter('#msg-loading').show();
-			var src = portal_url+"/@@markitup_redirect_uid?uid="+UID;
-//			parent.console.log(parent.markitup.format);
-			var formatStr = parent.markitup.format.Picture;
-			parent.$.markItUp({
-				replaceWith:formatStr.format(src, "Alternative text", title)
+			parent.jQuery.markItUp({
+				replaceWith: formatStr.format(src, "Alternative text", title)
 			});
 			if (Browser.forcecloseoninsert) {
 				parent.markitup.finder.overlay.close();
@@ -140,15 +163,19 @@ markitup = {
 		 * when the user selects an item MarkItUp knows what text to generate,
 		 * and then closes the iframe.
 		 */
-		selectItem: function (UID, title, selection) {
-			var statusBar = $(".statusBar > div", Browser.window);
-			var parent = window.parent;
-			if (window.opener) parent = window.opener;
+		selectItem: function (UID, title) {
+			"use strict";
+			var Browser = Browser || {},
+				statusBar = jQuery(".statusBar > div", Browser.window),
+				parent = window.parent,
+				href = portal_url + "/@@markitup_redirect_uid?uid=" + UID;
+			if (window.opener) {
+				parent = window.opener;
+			}
 			statusBar.hide().filter('#msg-loading').show();
-			var href = portal_url+"/@@markitup_redirect_uid?uid="+UID;
-			parent.$.markItUp({replaceWith:function(a) {
-				var formatStr = parent.markitup.format.Link;
-				return formatStr.format(href, a.selection, title)
+			parent.jQuery.markItUp({replaceWith: function (a) {
+				var formatStr = markitup.formatStr["text/" + parent.markitup.currentSet].Link;
+				return formatStr.format(href, a.selection, title);
 			}});
 			if (Browser.forcecloseoninsert) {
 				parent.markitup.finder.overlay.close();
@@ -176,17 +203,25 @@ markitup = {
 	 *          "Picture" and replace its "replaceWith", "className", and
 	 *          "beforeInsert" properties with the specified references.
 	 */
-	overrideSets: function(data, textStatus, jqXHR) {
-		var set_name = "text/" + markitup.currentSet;
-		if (set_name in data) {
-			var newSets = data[set_name];
-			for (var i=0; i<mySettings.markupSet.length; i++) {
-				// HACK: MarkItUp specifies that that the markupSets are in a
-				//       numeric array, not a hash indexed by name. Thus: Loop.
-				var curSet = mySettings.markupSet[i];
-				if (curSet.name in newSets) {
-					var newSet = newSets[curSet.name];
-					for (var key in newSet) {
+	overrideSets: function (data) {
+		"use strict";
+		var set_name = "text/" + markitup.currentSet,
+			newSets = data[set_name],
+			ctx = window,
+			key,
+			i = 0,
+			max = mySettings.markupSet.length,
+			curSet = {},
+			newSet = {},
+			s = "";
+		for (i = 0; i < max; i++) {
+			// HACK: MarkItUp specifies that that the markupSets are in a
+			//       numeric array, not a hash indexed by name. Thus: Loop.
+			curSet = mySettings.markupSet[i];
+			if (newSets.hasOwnProperty(curSet.name)) {
+				newSet = newSets[curSet.name];
+				for (key in newSet) {
+					if (newSet.hasOwnProperty(key)) {
 						if (newSet[key] === null) {
 							// Directly delete references to null.
 							delete curSet[key];
@@ -194,50 +229,55 @@ markitup = {
 							// HACK: Attempt to "guess" if a dotted name is intended to be a
 							//       reference to a Javascript object and not merely a string.
 							//       If only JSON had support for arbitrary references.
-							var ctx = window;
-							// NOTE: Loop has no body. Do everything in the loop expression.
-							for (var s=newSet[key].split("."); ctx&&s.length; ctx=ctx[s.shift()]);
-							curSet[key] = ctx||newSet[key];
+							for (s = newSet[key].split("."); ctx && s.length;) {
+								ctx = ctx[s.shift()];
+							}
+							curSet[key] = ctx || newSet[key];
 						}
 					}
 				}
 			}
 		}
-		$("#text").markItUp(mySettings);
-		$("textarea[id=form.text]").markItUp(mySettings);
+		jQuery("#text").markItUp(mySettings);
 	},
 	
-	setFormats: function(data, textStatus, jqXHR) {
-		markitup.format = data["text/" + markitup.currentSet];
+	setFormats: function (data) {
+		"use strict";
+		markitup.formatStr = data["text/" + markitup.currentSet];
 	},
 
 	/**
 	 * Change the editor based on the id of the current markup.
 	 * @param text_format {string} Name of the markup to which to switch
 	 */
-	setEditor: function(text_format) {
-		if (!(text_format && typeof text_format != "undefined")) return false;
+	setEditor: function (text_format) {
+		"use strict";
+		if (!(text_format && typeof text_format !== "undefined")) {
+			return false;
+		}
 		var subtype = text_format.split("/")[1];
 		markitup.unloadSet();
-		markitup.loadScript(markitup.base+subtype+"/set.js");
-		markitup.loadStyle(markitup.base+subtype+"/style.css");
+		markitup.loadScript(markitup.base + subtype + "/set.js");
+		markitup.loadStyle(markitup.base + subtype + "/style.css");
 		delete mySettings.previewTemplatePath;
 		mySettings.previewAutoRefresh = true;
 		mySettings.previewParserVar = text_format;
 		mySettings.previewParserPath = portal_url + '/@@markitup_preview';
 		markitup.currentSet = subtype;
-		$.getJSON(portal_url + "/@@markitup_json", {"name": "formats"}, markitup.setFormats);
-		$.getJSON(portal_url + "/@@markitup_json", {"name": "overrides"}, markitup.overrideSets );
+		jQuery.getJSON(portal_url + "/@@markitup_json", {"name": "formats"}, markitup.setFormats);
+		jQuery.getJSON(portal_url + "/@@markitup_json", {"name": "overrides"}, markitup.overrideSets);
+		
 	}
-}
+};
 
-// Attach MarkItUp editor to the main textarea on the body of a Plone edit
-// form.
-$(document).ready(function() {
-	markitup.loadScript(markitup.base+"markitup/jquery.markitup.js");
-	markitup.setEditor($("#text_text_format :selected").val());
-	$("#text_text_format").change(function(e) {
-		var text_format = $(this).find(":selected").val();
+// Attach MarkItUp editor to the JQuery object with an id of "#text", which
+// corresponds to the body editor in a normal Plone page.
+jQuery(document).ready(function () {
+	"use strict";
+	markitup.loadScript(markitup.base + "markitup/jquery.markitup.js");
+	markitup.setEditor(jQuery("#text_text_format :selected").val());
+	jQuery("#text_text_format").change(function () {
+		var text_format = jQuery(this).find(":selected").val();
 		markitup.setEditor(text_format);
 	});
 });
