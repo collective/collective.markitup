313e74456b616c7527f71e7253b87d6aee2c0cb6
MrTango <md@derico.de>
2014-08-05 22:22:07 +0200

    fix some pep8 violations

--- collective/markitup/browser/preview.py
+++ collective/markitup/browser/preview.py
@@ -2,16 +2,17 @@
 from zope.site.hooks import getSite
 from Products.Five.browser import BrowserView
 
+
 class Transform(BrowserView):
-	"""Preview the transform of Markup to HTML."""
+    """Preview the transform of Markup to HTML."""
 
-	def __init__(self, context, request):
-		super(Transform, self).__init__(context, request)
-		root = getSite().portal_url.getPortalPath()
-		self.js = root + "/++resource++collective.markitup/preview_styler.js"
+    def __init__(self, context, request):
+        super(Transform, self).__init__(context, request)
+        root = getSite().portal_url.getPortalPath()
+        self.js = root + "/++resource++collective.markitup/preview_styler.js"
 
-	def content(self):
-		"""Return the preview as a stringified HTML document."""
-		convertTo = getSite().portal_transforms.convertTo
-		mimetype, data = self.request.form.items()[0]
-		return convertTo("text/x-html-safe", data, mimetype=mimetype).getData()
+    def content(self):
+        """Return the preview as a stringified HTML document."""
+        convertTo = getSite().portal_transforms.convertTo
+        mimetype, data = self.request.form.items()[0]
+        return convertTo("text/x-html-safe", data, mimetype=mimetype).getData()
